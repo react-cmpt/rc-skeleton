b61f178caf8c1cac323868317ac16836d7518cae
wangcch <ci713@qq.com>
2019-07-05 19:19:51 +0800

    feat: update example

--- example/App.tsx
+++ example/App.tsx
@@ -6,9 +6,23 @@ import Skelenton from "../src";
 interface IProps {}
 
 export default function App(props: IProps) {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<string[]>([]);
+
   return (
     <div className={styleApp}>
-      <Skelenton />
+      <button onClick={() => setLoading(!loading)}>
+        loading: {`${loading}`}
+      </button>
+      <button onClick={() => (data.length > 0 ? setData([]) : setData(["a"]))}>
+        data: [{data.join(",")}]
+      </button>
+      <br />
+      <br />
+      <Skelenton loading={loading} emptyData={data.length <= 0}>
+        <div>render</div>
+      </Skelenton>
+      <br />
     </div>
   );
 }
